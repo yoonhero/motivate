aec810da2269404b144870fb0d0e48bf16e6d5c9
unknown <yoonhero06@naver.com>
2021-02-20 12:43:31 +0900

    2/20 ..

--- firstPage.js
+++ firstPage.js
@@ -18,10 +18,11 @@ function init(){
     var name = localStorage.getItem("name")
     var welcomeText = ["Good Morning, ", "Hello, ", "Welcome, ", "좋은 하루 되세요, ", "오늘도 힘내세요, ", "힘내요!! ", "어제보다 나은 오늘을 만들어요, "]
     if(name!= null){
-        document.querySelector(".page").classList.add("hidden")
         document.querySelector(".userName").innerText = name
         var index = Math.floor(Math.random() * welcomeText.length) 
         document.querySelector(".sayHello").innerText = welcomeText[index]
+    } else{
+        document.querySelector(".page").classList.remove("hidden")
     }
 }
 
